31fde5e036768786efad64d4b31e88d5fcd4d8f8
Mariengca27 <mariengca27>
2024-04-07 18:45:39 -0300

    Primeiro Commit - Estrutura base já limpa  do projeto

---
+++ ControleDeRegistros/Controllers/InformacoesController.cs
@@ -0,0 +1,15 @@
+﻿using ControleDeRegistros.Models;
+using Microsoft.AspNetCore.Mvc;
+using System.Diagnostics;
+
+
+namespace ControleDeRegistros.Controllers
+{
+    public class InformacoesController : Controller
+    {
+        public IActionResult Informacoes()
+        {
+            return View();
+        }
+    }
+}
---
+++ ControleDeRegistros/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace ControleDeRegistros.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
