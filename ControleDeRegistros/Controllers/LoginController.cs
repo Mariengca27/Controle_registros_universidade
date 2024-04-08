948cdaa5e172b38f0255f699fa3e717cdb791c54
Mariengca27 <mariengca27>
2024-04-08 14:19:41 -0300

    Alterações FrontEnd Layout e Login

---
+++ ControleDeRegistros/Controllers/LoginController.cs
@@ -0,0 +1,14 @@
+﻿using ControleDeRegistros.Models;
+using Microsoft.AspNetCore.Mvc;
+using System.Diagnostics;
+
+namespace ControleDeRegistros.Controllers
+{
+    public class LoginController : Controller
+    {
+        public IActionResult Login()
+        {
+            return View();
+        }
+    }
+}
